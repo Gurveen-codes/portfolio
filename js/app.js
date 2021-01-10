aeccf6db1a6a56782cd3b8b262b9fc5d53a81203
Gurveen <gurveens11@gmail.com>
2021-01-10 20:51:53 +0530

    Add Sticky Nav and minor style changes

--- js/app.js
+++ js/app.js
@@ -10,4 +10,20 @@ $(document).ready(function () {
 
     $(this).text(text.replace("CurrentYear", newDate));
   });
+
+  // /////////// Sticky Navigation Menu
+  let header_height = $(".header_area").height() + 50;
+
+  function navFixed() {
+    if ($(".header_area").length) {
+      let scroll = $(window).scrollTop();
+      if (scroll >= header_height) {
+        $(".header_area .main-menu").addClass("navbar_fixed");
+      } else {
+        $(".header_area .main-menu").removeClass("navbar_fixed");
+      }
+    }
+  }
+
+  $(window).on("scroll", navFixed);
 });
