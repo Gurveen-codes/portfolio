30b041f42d544a1b86ddd3ab343e0d236ede8d04
Gurveen <gurveens11@gmail.com>
2021-01-11 22:54:00 +0530

    Add navbar auto-update on scroll feature

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@ $(document).ready(function () {
   $(window).on("scroll", navFixed);
 
   /////////// Smooth Scrolling //////////////
-  $(".nav-link")
+  $(".nav-item")
     .toArray()
     .forEach((el) => {
       let link_id = `#${el["id"]}`;
@@ -41,6 +41,41 @@ $(document).ready(function () {
         $(target_area)[0].scrollIntoView({
           behavior: "smooth",
         });
+
+        $(".nav-item.active").removeClass("active");
+
+        $(this).addClass("active");
       });
     });
+
+  ///////////// Update Navbar based on scroll position /////////////
+
+  function updateNavbar() {
+    const currentPos = $(window).scrollTop();
+    //const headerPos = $(".header-area").position().top;
+    const aboutPos = $(".about-area").position().top;
+    const servicesPos = $(".services-area").position().top;
+    const skillsPos = $(".skills-area").position().top;
+    const projectPos = $(".project-area").position().top;
+    const footerPos = $(".footer-area").position().top;
+    const activeLink = $(".nav-item.active");
+
+    activeLink.removeClass("active");
+
+    if (currentPos < aboutPos) {
+      $("#home").addClass("active");
+    } else if (currentPos >= aboutPos && currentPos < servicesPos) {
+      $("#about").addClass("active");
+    } else if (currentPos >= servicesPos && currentPos < skillsPos) {
+      $("#services").addClass("active");
+    } else if (currentPos >= skillsPos && currentPos < projectPos) {
+      $("#skills").addClass("active");
+    } else if (currentPos >= projectPos && currentPos < footerPos - 200) {
+      $("#project").addClass("active");
+    } else {
+      $("#footer").addClass("active");
+    }
+  }
+
+  $(window).on("scroll", updateNavbar);
 });
